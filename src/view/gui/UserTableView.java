8aa4757dca95887f91d16badf0842d2eb243f33a
Janson-L <52344189+Janson-L@users.noreply.github.com>
2019-12-14 22:57:25 +0800

    Added row sorter

--- src/view/gui/UserTableView.java
+++ src/view/gui/UserTableView.java
@@ -21,6 +21,7 @@ public class UserTableView extends JFrame
         UserTableModel model = new UserTableModel(WedClothManager.searchUser());
         //create the table
         JTable table = new JTable(model);
+        table.setAutoCreateRowSorter(true);
          
         //add the table to the frame
         this.add(new JScrollPane(table));

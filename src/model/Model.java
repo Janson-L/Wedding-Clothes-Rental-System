3cb20e7d81fd2ca7be3fe21b8d9295b5e325c85b
Mervynnn <56059440+Mervynnn@users.noreply.github.com>
2019-12-14 13:25:04 +0800

    Revert "Revert "nah""
    This reverts commit 3b08224db189b3fff9a03f21aa665192beadf87e.

--- src/model/Model.java
+++ src/model/Model.java
@@ -11,38 +11,41 @@ public class Model
 	public static int getID(String searchType) throws ClassNotFoundException, SQLException
 	{
 		Class.forName("com.mysql.jdbc.Driver");
-		String id,tableName;
+		PreparedStatement ps;
 		
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/wed_cloth_management_system","root","");
+<<<<<<< HEAD
 		PreparedStatement ps= connection.prepareStatement("SELECT ? FROM ? ORDER BY ? DESC LIMIT 1;");
+=======
+>>>>>>> a8081f9d5cb7fcb2339cb02651ed6c2cf47c62de
 		if(searchType=="Clothes")
 		{
-			id="ClothesID";
-			tableName="Clothes";
+			ps= connection.prepareStatement("SELECT ClothesID FROM Clothes ORDER BY ClothesID DESC LIMIT 1;");
 		}
 		else if(searchType=="User")
 		{
-			id="UserID";
-			tableName="User";
+			ps= connection.prepareStatement("SELECT UserID FROM User ORDER BY UserID DESC LIMIT 1;");
 		}
 		else if(searchType=="Rental")
 		{
-			id="RentalID";
-			tableName="Rental";
+			ps= connection.prepareStatement("SELECT RentalID FROM Rental ORDER BY RentalID DESC LIMIT 1;");
 		}
 		else if(searchType=="Payment")
 		{
-			id="PaymentID";
-			tableName="Payment";
+			ps= connection.prepareStatement("SELECT PaymentID FROM Payment ORDER BY PaymentID DESC LIMIT 1;");
 		}
 		else {return -1;}
 		
+<<<<<<< HEAD
 		ps.setString(1, id);
 		ps.setString(2, tableName);
 		ps.setString(3, id);
 
+=======
+>>>>>>> a8081f9d5cb7fcb2339cb02651ed6c2cf47c62de
 		ResultSet rs=ps.executeQuery();
 		connection.close();
+		
 		int idDB=0;
 		if(!rs.isBeforeFirst()) {
 			idDB=1;
